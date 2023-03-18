1db59feae714ecc34290ae3c74122260cabeb737
Arni Magnusson <thisisarni@gmail.com>
2023-03-19 04:46:52 +1100

    Draft data and software

--- 09_2015_rjm-347d_draft/test.R
+++ 09_2015_rjm-347d_draft/test.R
@@ -1,5 +1,9 @@
 library(icesTAF)
 
+draft.data(originator="Arlin", year=2000, file=TRUE)
+draft.software("arni-magnusson/areaplot@1.2.3", file=TRUE)
+draft.software("bootstrap/initial/software/catageysa", file=TRUE, append=TRUE)
+
 taf.boot()
 print.simple.list(dir(boot.dir(), full=TRUE, recursive=TRUE))
 
@@ -8,3 +12,5 @@ print.simple.list(dir("report"))
 
 clean()
 clean.boot(force=TRUE)
+git <- system("git checkout -- bootstrap/DATA.bib")
+unlink("bootstrap/SOFTWARE.bib")
